c8ad08ea1744c6ee406203ee536e48a2d8688248
Methmina Binara <binaraepa@gmail.com>
2025-04-06 00:29:57 +0530

    Fixed play and secret rooms with App Router data

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,8 +1,27 @@
-export default function PlayPage() {
+type Toy = {
+    id: number;
+    name: string;
+};
+
+export default async function PlayPage() {
+
+    const toys: Toy[] = [
+        { id: 1, name: "Toy Car" },
+        { id: 2, name: "Buiding Blocks" },
+        { id: 3, name: "Stuffed Bear"}
+    ];
+
     return (
         <div className="flex flex-col gap-2">
             <h1 className="text-green-500">My Play Room</h1>
             <p>This is where I have fun with toys</p>
+            <ul  className="list-disc pl-5">
+                {toys.map((toy) => (
+                    <li key={toy.id} className="text-green-300">
+                        {toy.name}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
--- src/app/secret/page.tsx
+++ src/app/secret/page.tsx
@@ -1,8 +1,28 @@
-export default function SecretPage() {
+type Treasure = {
+    id: number;
+    secret: string;
+};
+
+export default async function SecretPage() {
+
+    const treasures: Treasure[] = [
+        { id: 1, secret: "Magic Wand" },
+        { id: 2, secret: "Invisible Cape" },
+        { id: 3, secret: "Golden Key" },
+    ];
+
     return (
         <div className="flex flex-col gap-2">
             <h1 className="text-yellow-500">My Secret Room</h1>
-            <p>This is where I keep my hidden toy stash!</p>
+            <p>Shh! This is where I keep my hidden toy stash!</p>
+            <ul className="list-disc pl-5">
+                {treasures.map((treasure) => (
+                    <li key={treasure.id} className="text-yellow-300">
+                        {treasure.secret}
+                    </li>
+                ))}
+
+            </ul>
         </div>
     );
 }
