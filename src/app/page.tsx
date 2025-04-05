14f16a176ced920909fce104d2a8613af6e3bcf2
Methmina Binara <binaraepa@gmail.com>
2025-04-05 18:28:42 +0530

    Added play and secret rooms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col gap-2'>
       <h1 className="text-blue-500">My Cool Toy House!</h1>
       <p>Welcome to my site</p>
-      <Link href="/play" className="text-purple-500">Go to play Room</Link>
+      <Link href="/play" className="text-purple-500">Go to Play Room</Link>
+      <Link href="/secret" className="text-rose-500">Go to Secret Room</Link>
     </div>
   );
 }
--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,8 +1,8 @@
 export default function PlayPage() {
     return (
-        <div className="text-green-500">
-            <h1>My Play Room</h1>
-            <p>This is where I have fun</p>
+        <div className="flex flex-col gap-2">
+            <h1 className="text-green-500">My Play Room</h1>
+            <p>This is where I have fun with toys</p>
         </div>
     );
 }
