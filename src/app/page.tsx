e028cd936f77da2e097d3459fc64ba33965649f4
Methmina Binara <binaraepa@gmail.com>
2025-04-05 17:32:29 +0530

    Added a play room and link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <h1 className="text-blue-500">My Cool Toy House!</h1>
+    <div>
+      <h1 className="text-blue-500">My Cool Toy House!</h1>
+      <p>Welcome to my site</p>
+      <Link href="/play" className="text-purple-500">Go to play Room</Link>
+    </div>
   );
 }
---
+++ src/app/play/page.tsx
@@ -0,0 +1,8 @@
+export default function PlayPage() {
+    return (
+        <div className="text-green-500">
+            <h1>My Play Room</h1>
+            <p>This is where I have fun</p>
+        </div>
+    );
+}
